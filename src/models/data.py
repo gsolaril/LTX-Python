0aef779294c3ae735e32c7d09c154be3593d71f7
Gaston Solari Loudet <gsolaril@itba.edu.ar>
2026-06-12 12:06:02 +0000

    Fixed some little stuff from models

--- src/models/data.py
+++ src/models/data.py
@@ -8,94 +8,7 @@ from pandas import Timestamp, Timedelta
 from pandas import Series, DataFrame, concat
 from misc import Symbol, TimeFrame
 from src.utils import Log
-#▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-#███████████████████████████████████████████████████████████████████████████████████████████████████████████
-#▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-#▄▄▄▄▄▄▄▄▄
-@dataclass
-class Order:
-    venue: str; symbol: str; size: float
-    price: float = None; comment: str = None
-    expiration: Timestamp = None
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def __post_init__(self):
-        self.symbol = self.symbol.upper()
-        self.symbol = self.symbol.replace("/", "")
-        self.symbol = self.symbol.replace(":", "")
-        assert (self.size != 0), "Order size cannot be 0"
-        self.side = "BUY" if (self.size > 0) else "SELL"
-        self.type = "LIMIT" if self.price else "MARKET"
-        self.mode = "GTC" if self.price else "IOC"
-        if not self.comment: self.comment = ""
-        
-        self.time = Timestamp.utcnow()
-        ts = int(self.time.timestamp() * 1e6)
-        self.UID = numpy.base_repr(ts, base = 36).upper()
-        self._check_expired(self.time)
-        
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def _check_expired(self, time: Timestamp):
-        if self.expiration is not None:
-            error = "Order already expired..." \
-            f"\n => (NOW) {time:%Y/%m/%d %H:%M:%S} > " \
-            f"(EXP) {self.expiration:%Y/%m/%d %H:%M:%S}"
-            assert time < self.expiration, error
 
-    #▄▄▄▄▄▄▄▄▄▄
-    @property#█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def __dict__(self): return asdict(self)
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def __repr__(self): return self.inline()
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def inline(self):
-        verbose = "Order({venue} {symbol}, S{size:+} P{price} @ "
-        verbose += "{time:%Y/%m/%d %H:%M:%S.%f}, E+{DE:.1f}s | {UID})"
-        if self.expiration is None: expires_in = numpy.inf
-        else: expires_in = Timedelta.total_seconds(self.expiration - self.time)
-        return verbose.format(**self.__dict__, UID = self.UID,
-                             time = self.time, DE = expires_in)
-#▄▄▄▄▄▄▄▄▄▄▄
-@dataclass#█▄▄▄▄▄▄▄▄▄
-class Response(Order):
-    status: str = None
-    EID: str = None; UID: str = None
-    time_place: Timestamp = None
-
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def __init__(self, order: Order, **kwargs):
-        super().__init__(**order.__dict__)
-        self.time_order = order.time
-        self.size = float(kwargs.get("size", order.size))
-        self.price = float(kwargs.get("price", order.price))
-        if self.time_place is None:
-            self.time_place = Timestamp.utcnow()
-        
-        self._check_expired(self.time_order)
-        self._check_expired(self.time_place)
-        self.status = kwargs["status"]
-        self.EID = kwargs["EID"]
-        self.UID = order.UID
-        self.time = order.time
-
-    #▄▄▄▄▄▄▄▄▄▄
-    @property#█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def __dict__(self): return {**asdict(self),
-        "time_order": self.time_order}
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def __repr__(self): return self.inline(4)
-    #▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    def inline(self, nlspace: int = None):
-        if (nlspace is None): sep = ", IDs: "
-        else: sep = "\n" + " " * nlspace
-        verbose = "Order({venue} {symbol}, S{size:+} P{price} @ "
-        if self.expiration is None: expires_in = numpy.inf
-        else: expires_in = Timedelta.total_seconds(self.expiration - self.time_order)
-        delay = 1e6 * Timedelta.total_seconds(self.time_place - self.time_order)
-        verbose += "{time:%Y/%m/%d %H:%M:%S.%f}, D+{DD:.0f}µs, E+{DE:.1f}s{sep}{UID} {EID} | {status})"
-        return verbose.format(**self.__dict__, sep = sep,
-          time = self.time_order, DD = delay, DE = expires_in)
-
-#▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 #███████████████████████████████████████████████████████████████████████████████████████████████████████████
 #▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 #▄▄▄▄▄▄▄▄▄
@@ -128,7 +41,7 @@ class Tick:
         time = f"{self.time:%Y/%m/%d %H:%M:%S.%f}"
         return f"Tick(@ {time}) | {self.venue}.{self.symbol} | " \
                 f"A:{self.pa}/{self.qa}, B:{self.pb}/{self.qb})"
-#▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+
 #███████████████████████████████████████████████████████████████████████████████████████████████████████████
 #▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 #▄▄▄▄▄▄▄▄▄
@@ -219,7 +132,6 @@ class Candle:
           ca = candle.ca, cb = candle.cb, volume = 0, time = candle._time_close,
           oa = None, ob = None)
 
-#▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 #███████████████████████████████████████████████████████████████████████████████████████████████████████████
 #▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 #▄▄▄▄▄▄▄▄▄▄▄
