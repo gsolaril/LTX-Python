2e6398368c2841a4a70438b5fa333bfd0e6e2684
Gaston Solari Loudet <gsolaril@itba.edu.ar>
2026-06-12 12:34:39 +0000

    Connectors' base started

---
+++ src/connectors/base.py
@@ -0,0 +1,10 @@
+#▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+import asyncio, json
+from typing import Any, Dict, List, Callable, Optional
+from aiohttp import WSMessage, WSMsgType, ClientSession
+from aiohttp import ClientWebSocketResponse
+from src.models import *
+from src.utils import *
+
+#███████████████████████████████████████████████████████████████████████████████████████████████████████████
+#▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
