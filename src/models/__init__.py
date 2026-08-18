e4645c2cad463ab83d515ea727e3f204a3482150
Gaston Solari Loudet <gsolaril@itba.edu.ar>
2026-08-18 04:57:03 +0000

    Allowed ExecReceiver to have multiple accounts

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 #▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-from .account import Account, Account, Rules
+from .account import Account, AccountDict, Rules
 from .agent import BaseAgent, StreamingAgent, ControllableAgent
 from .bundle import Bundle, TestBundle
 from .data import BasePoint, DataPoint, Tick, Candle, Quote, QuoteDict
@@ -10,6 +10,6 @@ tests = {
     "models/bundle": TestBundle
 }
 __all__ = ["tests", "OrderCreate", "OrderModify", "OrderDelete", "OrderReject", "Order", "Message",
-    "Trade", "Symbol", "SymbolDict", "Account", "Rules", "TimeFrame", "BaseAgent", "StreamingAgent",
-    "ControllableAgent", "BasePoint", "DataPoint", "Account", "Quote", "Tick", "Candle",  "Bundle",
-    "OrderDict", "TradeDict", "QuoteDict"]
+    "Trade", "Symbol", "SymbolDict", "Account", "AccountDict", "Rules", "TimeFrame", "BaseAgent",
+    "StreamingAgent", "ControllableAgent", "BasePoint", "DataPoint", "Quote", "Tick", "Candle",
+    "Bundle", "OrderDict", "TradeDict", "QuoteDict"]
